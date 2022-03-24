a47b727d2908c08336565cbbe53f29ff2cee4cae
mediumcavalry <77861842+zacharykollar@users.noreply.github.com>
2022-03-23 22:14:09 -0400

    Import jsoup scrape wiki table

--- Main.java
+++ Main.java
@@ -1,20 +1,39 @@
 
 import java.io.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 public class Main {
     public static void main (String[] args){
-        writeToFile("never");
+        final String url ="https://en.wikipedia.org/wiki/Guinea_pig";
+        getData(url);
     }
     public static void getData(String url){
+        try {
+            final Document doc = Jsoup.connect(url).get();
+            FileWriter out = new FileWriter("output.txt");
+            BufferedWriter writer = new BufferedWriter(out);
+            writer.write(doc.title());
+            //writeToFile(doc.title());
+            for (org.jsoup.nodes.Element row : doc.select("table.infobox.biota tr")){
+                if(row.text() == "")
+                      continue;
+                writer.write("\n" + row.text());
+                System.out.println(row.text());
+            }
+            writer.close();
+        } catch (Exception e){
 
+        }
     }
     public static void writeToFile(String data){
         try {
             FileWriter out = new FileWriter("output.txt");
             BufferedWriter writer = new BufferedWriter(out);
             writer.write(data);
+            writer.newLine();
             writer.close();
         } catch (Exception e) {
-            System.out.print("file failed");
+            System.out.print("write to file failed");
         }
     }
 }
