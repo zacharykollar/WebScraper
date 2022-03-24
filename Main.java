d15d158079d6d8705def8afcd83a55063739bbb9
mediumcavalry <77861842+zacharykollar@users.noreply.github.com>
2022-03-24 18:33:27 -0400

    Added maxDepth parameter to findDepth

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ import org.jsoup.nodes.Document;
 public class Main {
     public static void main (String[] args){
         final String url ="https://en.wikipedia.org/wiki/Guinea_pig";
-        final String targetUrl ="https://en.wikipedia.org/wiki/Jaw";
+        final String targetUrl ="https://en.wikipedia.org/wiki/Even-toed_ungulate";
         try {
-            writeToFile(String.valueOf(findDepth(Jsoup.connect(url).get(), targetUrl, url)));
+            writeToFile(String.valueOf(findDepth(Jsoup.connect(url).get(), targetUrl, url, 2)));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -29,17 +29,17 @@ public class Main {
 
         }
     }
-    public static int findDepth(Document doc, String target, String url){
-        return findDepthHelper(doc, target, url, 0, 300);
+    public static int findDepth(Document doc, String target, String url, int maxDepth){
+        return findDepthHelper(doc, target, url, maxDepth, 0, 300);
     }
-    public static int findDepthHelper(Document doc, String target, String url, int deep, int smallest){
+    public static int findDepthHelper(Document doc, String target, String url, int maxDepth, int deep, int smallest){
         System.out.println(deep + " : " + url + " : " + smallest);
         if (url.equals(target))
             return deep;
-        if (deep >= 5 || deep >= smallest)
+        if (deep >= maxDepth || deep >= smallest)
             return 200;
         for (org.jsoup.nodes.Element link : doc.select("[href]")){
-            int d = findDepthHelper(doc, target, link.attr("abs:href"), deep + 1, smallest);
+            int d = findDepthHelper(doc, target, link.attr("abs:href"), maxDepth, deep + 1, smallest);
             if (smallest > d)
                 smallest = d;
         }
