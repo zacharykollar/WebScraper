3d86b2acbce53df5656e8c951687d5bc3aac3bd4
mediumcavalry <77861842+zacharykollar@users.noreply.github.com>
2022-03-24 11:59:39 -0400

    findDepth to findDepthHelper
    findDepth is now generic function with deep and smallest elements removed that uses findDepthHelper which contains the original functionality

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         final String url ="https://en.wikipedia.org/wiki/Guinea_pig";
         final String targetUrl ="https://en.wikipedia.org/wiki/Jaw";
         try {
-            writeToFile(String.valueOf(findDepth(Jsoup.connect(url).get(), targetUrl, url, 0, 300)));
+            writeToFile(String.valueOf(findDepth(Jsoup.connect(url).get(), targetUrl, url)));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -29,14 +29,17 @@ public class Main {
 
         }
     }
-    public static int findDepth(Document doc, String target, String url, int deep, int smallest){
+    public static int findDepth(Document doc, String target, String url){
+        return findDepthHelper(doc, target, url, 0, 300);
+    }
+    public static int findDepthHelper(Document doc, String target, String url, int deep, int smallest){
         System.out.println(deep + " : " + url + " : " + smallest);
         if (url.equals(target))
             return deep;
         if (deep >= 5 || deep >= smallest)
             return 200;
         for (org.jsoup.nodes.Element link : doc.select("[href]")){
-            int d = findDepth(doc, target, link.attr("abs:href"), deep + 1, smallest);
+            int d = findDepthHelper(doc, target, link.attr("abs:href"), deep + 1, smallest);
             if (smallest > d)
                 smallest = d;
         }
