2a031fdfbef9067cb706ad48f94ee8fb5268f25a
mediumcavalry <77861842+zacharykollar@users.noreply.github.com>
2022-03-29 15:26:45 -0400

    created breadth first methods, nonfunctional

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import org.jsoup.nodes.Document;
 public class Main {
     public static void main (String[] args){
         final String url ="https://en.wikipedia.org/wiki/Guinea_pig";
-        final String targetUrl ="https://en.wikipedia.org/wiki/Even-toed_ungulate";
+        final String targetUrl ="https://en.wikipedia.org/wiki/Jaw";
         try {
             writeToFile(String.valueOf(findDepth(Jsoup.connect(url).get(), targetUrl, url, 2)));
         } catch (IOException e) {
@@ -33,10 +33,10 @@ public class Main {
         return findDepthHelper(doc, target, url, maxDepth, 0, 300);
     }
     public static int findDepthHelper(Document doc, String target, String url, int maxDepth, int deep, int smallest){
-        System.out.println(deep + " : " + url + " : " + smallest);
+        //System.out.println(deep + " : " + url + " : " + smallest);
         if (url.equals(target))
             return deep;
-        if (deep >= maxDepth || deep >= smallest)
+        if (deep > maxDepth || deep >= smallest)
             return 200;
         for (org.jsoup.nodes.Element link : doc.select("[href]")){
             int d = findDepthHelper(doc, target, link.attr("abs:href"), maxDepth, deep + 1, smallest);
@@ -45,6 +45,15 @@ public class Main {
         }
         return smallest;
     }
+
+    //uses breadth first search with hashmaps
+    public static int findDepthBreadth(Document doc, String target, String url, int maxDepth){
+        return findDepthBreadthHelper(doc, target, url, maxDepth, 0, 300);
+    }
+    private static int findDepthBreadthHelper(Document doc, String target, String url, int maxDepth, int deep, int smallest) {
+        return 0;
+    }
+    //tools
     public static void writeToFile(String data){
         try {
             FileWriter out = new FileWriter("output.txt");
