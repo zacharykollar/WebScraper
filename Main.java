b5beecbfca90beb7562eae25d425dffd52d8389d
mediumcavalry <77861842+zacharykollar@users.noreply.github.com>
2022-03-31 15:17:39 -0400

    Fixed overreach error in fDB

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         final String url = "https://en.wikipedia.org/wiki/Guinea_pig";
-        final String target = "https://en.wikipedia.org/wiki/Jaw";
+        final String target = "https://en.wikipedia.org/wiki/Capybara";
         writeToFile(String.valueOf(findDepthBreadth(target, url, 2)));
     }
 
@@ -74,7 +74,7 @@ public class Main {
         if (url.equals(target))
             return deep;
         second.addAll(getLinksHash(url));
-        while (deep < maxDepth) {
+        while (deep < maxDepth - 1) {
             deep++;
             System.out.println(deep + " : " + first.size() + " : " + second.size());
             if (deep % 2 == 1) {
